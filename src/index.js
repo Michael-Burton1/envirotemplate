1e225dd1d4d638e613b2a33d248e886878b83bcf
Michael Burton <micbur1@gmail.com>
2021-04-17 18:49:02 -0700

    more files for enviroment. I know this is a terrible commit message

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// import { Class } from './class-name.js';   this is what to use if importing muiltiple files
-import Class from './class-name.js';       //this is usedm to import 1 default file
---
+++ src/main.js
@@ -0,0 +1,4 @@
+// import { Class } from './class-name.js';   this is what to use if importing muiltiple files
+import Class from './class-name.js';       //this is usedm to import 1 default file
+
+import '../css/styles.css';
